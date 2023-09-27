7fa6dd5d64e451d88e8d0389e16c8e0591b6a18e
md_shoeb <mdshoebofficial2050@gmail.com>
2023-09-27 17:58:25 +0530

    object file added

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,38 +1,99 @@
 // singleton
-// Object.create
+// Object.create  // constructor method to create objects - singleton method
+
+/*
+In JavaScript, objects are king. If you understand objects, you understand JavaScript.
+
+In JavaScript, almost "everything" is an object.
+
+Booleans can be objects (if defined with the new keyword)
+Numbers can be objects (if defined with the new keyword)
+Strings can be objects (if defined with the new keyword)
+Dates are always objects
+Maths are always objects
+Regular expressions are always objects
+Arrays are always objects
+Functions are always objects
+Objects are always objects
+
+All JavaScript values, {except primitives}, are objects.
+
+// *** >> JavaScript Primitives
+A primitive value is a value that has no properties or methods.
+3.14 is a primitive value
+
+A primitive data type is data that has a primitive value.
+// >>>JavaScript defines 7 types of primitive data types:
+
+Examples
+    string
+    number
+    boolean
+    null
+    undefined
+    symbol
+    bigint
+*/
 
 // object literals
 
+
+
+// key : value  => both are string in object behind the scene strign is considered
+// you can make also nedted objects
+
+//creating a symbol and add to this into obejct to make symbol as a key
 const mySym = Symbol("key1")
-
-
+//creating an object
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    name: "Shoeb",
+    "full name": "Shoeb Ansari",
+    [mySym]: "mykey1",  // here i have used symbol as a key [mySym]
     age: 18,
-    location: "Jaipur",
-    email: "hitesh@google.com",
+    location: "Varanasi",
+    email: "mdshoeb1024@gmail.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-// console.log(JsUser.email)
-// console.log(JsUser["email"])
-// console.log(JsUser["full name"])
-// console.log(JsUser[mySym])
+// console.log(JsUser.email)   // 1st way to access obejects value
+// console.log(JsUser["email"])  // 2nd way to access obejects value {square notation method}
 
-JsUser.email = "hitesh@chatgpt.com"
-// Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+// console.log(JsUser["full name"]) //this is the best practice 
+
+// console.log(JsUser[mySym])  //mykey1   - * interview things
+// console.log( typeof JsUser[mySym])  //string
+
+
+// * Interview Q - Creatre a symbol make that key and access that value 
+JsUser.email = "mdshoeb1024@gmail.com"
+// Object.freeze(JsUser)  // value freez - ab aage koi bhi new value propogate nahi hoga iske andar
+JsUser.email = "mdshoeb1024@microsoft.com"
+// console.log(JsUser);   // email: 'mdshoeb1024@gmail.com',
+
+/*
+{
+  name: 'Shoeb',
+  'full name': 'Shoeb Ansari',
+  age: 18,
+  location: 'Varanasi',
+  email: 'mdshoeb1024@gmail.com',
+  isLoggedIn: false,
+  lastLoginDays: [ 'Monday', 'Saturday' ],
+  [Symbol(key1)]: 'mykey1'          // here it refers that key is symbol, not a normal key 
+}
+*/
 
 JsUser.greeting = function(){
-    console.log("Hello JS user");
+    // console.log("Hello JS user");
 }
 JsUser.greetingTwo = function(){
-    console.log(`Hello JS user, ${this.name}`);
+    console.log(`Hello JS user, ${this.name}`);  // same object ko refer karna using this keyword
 }
 
-console.log(JsUser.greeting());
-console.log(JsUser.greetingTwo());
+// console.log(JsUser.greeting);  //[Function (anonymous)]  - fun execute nahi hua h sirf uska reference aaya h
+// console.log(JsUser.greeting());   // undefined
+
+// console.log(JsUser.greetingTwo()); 
+//Hello JS user, Shoeb 
+// undefined  -> ye extra line execute hua h browser me bhi hota h - have to study in upcoming lecture 
