adc5231d303e7d9b935b68c06c19c7ac3a8c7e3c
md_shoeb <mdshoebofficial2050@gmail.com>
2023-10-03 22:56:50 +0530

    one.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,12 +1,35 @@
+/****************************
+ *      Lecture - 24
+ * -Immediately Invoked Function Expressions (IIFE)
+ * 
+ ***************************/
+
+// To avoid pollution of Global Scope (jo bhi varibles declard h ) - we should use IIFE technique in our code
 // Immediately Invoked Function Expressions (IIFE)
 
+// () ()  
+// () - fun definition
+// ()  - Execution call
+
+ 
 
 (function chai(){
-    // named IIFE
+    // named IIFE - It means you are using fun name 
     console.log(`DB CONNECTED`);
 })();
 
+// iife ke curent context ko STOP karne ke liye ; ka use karenge 
+
+
+//Simple IIFE - not using fun name 
+(()=>{
+    console.log(`DB CONNECTED SUCCESSFULLY`);
+})(); // 2 iife likhte time 1st wale iife ke bad ; lagana must h
+
+ 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
-} )('hitesh')
+} )('by mdShoeb')  // Here i have passed parameter to execution call  
+
+// chai('shoeb')  // I have used this into execution call 
 
