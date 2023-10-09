fabb38cd821958b35121f0075e5ff94641524414
md_shoeb <mdshoebofficial2050@gmail.com>
2023-10-09 21:20:50 +0530

    2projects completed

---
+++ 0_Projects/Random_color/script.js
@@ -0,0 +1,49 @@
+//Task - Generate a Random color 
+
+const randColor= function (){
+    // hex value range from 0 to 9 and A -F
+    const hex='0123456789ABCDEF'
+    let color = '#'      // taking # bcoz color start with #
+    // to make 6 digit number using loop
+    for(let i=0; i<6; i++){
+        //values constantly add karni h
+        // ith value nahi select karna h, Random value select karna h
+        color += hex[Math.floor(Math.random()*16)]
+    }
+   return color;
+};
+
+let intervalId; // global scope 
+
+const startChangingColor = function () {
+   //safety check for NULL
+   if(!intervalId){
+       intervalId = setInterval(changeBgcolor,1000);
+   }
+
+  // To run continuously use method 
+   function changeBgcolor(){
+    document.body.style.backgroundColor= randColor();
+   }
+ 
+
+};
+
+const stopChangingColor = function () {
+    clearInterval(intervalId);
+    // tip : after clearInterval the best practice is to make intervalId is NULL ( dereference)
+    // tip : professioalism  - bar bar kisi variable ko override karna thik nahi after use set it as NULL
+    intervalId = null;   // agar varible ka refrence use nahi aa rha to NULL set kar denge so that automatically thodi memory free ho jaye
+};
+
+// taking reference of a function 
+document.querySelector('#start').addEventListener
+('click', startChangingColor);
+
+document.querySelector('#stop').addEventListener
+('click', stopChangingColor);
+
+
+// tip : try to fid and Learn  corner case (Edge case)  this will make you a good developer  
+
+//32;05 - second project
