c3e4c634a357aa4f1ebcb19529e3ab50d8025997
md_shoeb <mdshoebofficial2050@gmail.com>
2023-12-15 19:08:14 +0530

    Module-6 added

---
+++ JS_by_CN/05_Module(calculator)/script.js
@@ -0,0 +1,32 @@
+// javascript code here
+let buttons = document.querySelector('.buttons');
+let btn = document.querySelectorAll('span');
+let value = document.getElementById('value');
+
+// toggle button
+let toggleBtn = document.querySelector('.toggleBtn');
+let body = document.querySelector('body');
+
+
+for(let i=0; i<btn.length; i++){
+    btn[i].addEventListener("click",function(){
+
+        //checking condition
+        if(this.innerHTML== "="){
+            value.innerHTML = eval(value.innerHTML);
+        }
+        else 
+        {
+            if(this.innerHTML == "Clear"){
+                value.innerHTML = "";
+            }
+            else{
+                value.innerHTML += this.innerHTML;
+            }
+        }
+    });
+}
+
+toggleBtn.onclick = function(){
+    body.classList.toggle('dark');
+}
---
+++ JS_by_CN/06_Module(Mini Projects)/topics.md
@@ -0,0 +1,42 @@
+# Module -6
+
+### Mini Projects Using Javascript
+
+1. **Const** 
+    - Summary Notes  
+    - 
+2. **Let**
+    - Summary Notes
+    -  
+3. **Let vs Var**
+    - Summary Notes 
+    - 
+4. **For of loop**
+   - Summary Notes 
+   - 
+5. **Resume Smooth Scroll feature**
+   - Summary Notes 
+   - 
+6. **Resume Auto fill Skill Bars feature**
+   - Summary Notes
+   -  
+7. **Counter feature**
+   - Summary Notes 
+   - 
+8. **Assignments**
+    1. **Move the Ball**
+       - Summary Notes 
+       -     
+       -     
+    2. **Catch me If You Can**
+       - Summary Notes 
+       -     
+       -    
+    3. **Percentage Scrolled**
+       - Summary Notes 
+       -     
+       -     
+    4. **Five Digit Up Counter**
+       - Summary Notes 
+       -     
+       -    
